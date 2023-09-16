b80ad49140be7679323b311d91770855968a5573
matistjati <matistjati@outlook.com>
2023-09-16 11:05:13 +0200

    Statement fixes

--- onlinekval/pariserhjulet/submissions/accepted/eae.py
+++ onlinekval/pariserhjulet/submissions/accepted/eae.py
@@ -1,6 +1,6 @@
 from heapq import *
-N, M = map(int, raw_input().split(" "))
+N, M = map(int, input().split(" "))
 queue = list(range(M))
-for turns in map(int, raw_input().split(" ")):
+for turns in map(int, input().split(" ")):
 	heappush(queue, heappop(queue) + turns * M)
 print(max(queue))
